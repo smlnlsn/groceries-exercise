622d26dc0c807268852ba7e2b3d883790327c25d
smlnlsn <59628711+smlnlsn@users.noreply.github.com>
2020-01-27 15:28:17 -0500

    Update groceries.py

--- groceries.py
+++ groceries.py
@@ -18,7 +18,7 @@ products = [
     {"id":13, "name": "Saline Nasal Mist", "department": "personal care", "aisle": "cold flu allergy", "price": 16.00},
     {"id":14, "name": "Fresh Scent Dishwasher Cleaner", "department": "household", "aisle": "dish detergents", "price": 4.99},
     {"id":15, "name": "Overnight Diapers Size 6", "department": "babies", "aisle": "diapers wipes", "price": 25.50},
-    {"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
+    #{"id":16, "name": "Mint Chocolate Flavored Syrup", "department": "snacks", "aisle": "ice cream toppings", "price": 4.50},
     {"id":17, "name": "Rendered Duck Fat", "department": "meat seafood", "aisle": "poultry counter", "price": 9.99},
     {"id":18, "name": "Pizza for One Suprema Frozen Pizza", "department": "frozen", "aisle": "frozen pizza", "price": 12.50},
     {"id":19, "name": "Gluten Free Quinoa Three Cheese & Mushroom Blend", "department": "dry goods pasta", "aisle": "grains rice dried goods", "price": 3.99},
@@ -28,4 +28,41 @@ products = [
 print(products)
 # pprint(products)
 
+#breakpoint()
+
+print("--------------------")
+#print("THERE ARE", len(products), "PRODUCTS:")
+#print("THERE ARE " + str(len(products)) + " PRODUCTS:")
+print(f"THERE ARE {len(products)} PRODUCTS:")
+print("--------------------")
+  
+for item in products:
+    print(item["name"])
+
+
+
 # TODO: write some Python code here to produce the desired output
+
+#  --------------
+#  THERE ARE 20 PRODUCTS:
+#  --------------
+#   + All-Seasons Salt ($4.99)
+#   + Chocolate Fudge Layer Cake ($18.50)
+#   + Chocolate Sandwich Cookies ($3.50)
+#   + Cut Russet Potatoes Steam N' Mash ($4.25)
+#   + Dry Nose Oil ($21.99)
+#   + Fresh Scent Dishwasher Cleaner ($4.99)
+#   + Gluten Free Quinoa Three Cheese & Mushroom Blend ($3.99)
+#   + Green Chile Anytime Sauce ($7.99)
+#   + Light Strawberry Blueberry Yogurt ($6.50)
+#   + Mint Chocolate Flavored Syrup ($4.50)
+#   + Overnight Diapers Size 6 ($25.50)
+#   + Peach Mango Juice ($1.99)
+#   + Pizza For One Suprema Frozen Pizza ($12.50)
+#   + Pomegranate Cranberry & Aloe Vera Enrich Drink ($4.25)
+#   + Pure Coconut Water With Orange ($3.50)
+#   + Rendered Duck Fat ($9.99)
+#   + Robust Golden Unsweetened Oolong Tea ($2.49)
+#   + Saline Nasal Mist ($16.00)
+#   + Smart Ones Classic Favorites Mini Rigatoni With Vodka Cream Sauce ($6.99)
+#   + Sparkling Orange Juice & Prickly Pear Beverage ($2.99)
